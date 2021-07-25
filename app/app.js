30188ddf7490d41c672587bb7274574e84d88552
WhatUpItsDanielle <danielle.davidson98@gmail.com>
2021-07-24 22:47:34 -0700

    Javascript skeleton complete. Functions need to be filled in and styled still

---
+++ app/app.js
@@ -0,0 +1,19 @@
+const budgets = [
+    {name: "Savings", item:"", total:""}
+]
+
+const bttn = document.querySelector("#new")
+bttn.addEventListener('click', card);
+
+function card(){
+    //creates area for new card to have input given
+    //
+
+}
+
+function toggle(){
+    //hides and un-hides either the ticket screen or the individual editing screen
+    //individual: input feilds for name and items, shows total and delete button
+    //all: shows all cards
+}
+
